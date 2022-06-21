a7a4068f8e181705a425bff349170a5638369b4f
rancharaz <ranveer1990@hotmail.com>
2022-06-22 00:49:13 +0400

    delete button, reuse getData

--- dashboard/ecommerce-dashboard/src/components/ProductList.jsx
+++ dashboard/ecommerce-dashboard/src/components/ProductList.jsx
@@ -11,6 +11,22 @@ const ProductList = () => {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
+        getData()
+    }, [])
+
+
+    async function deleteOperation(id) {
+
+        let result = await fetch("http://127.0.0.1:8000/api/delete/" + id, {
+            method: "DELETE"
+        })
+
+        result = await result.json();
+        console.table(result);
+        getData()
+    }
+
+    function getData() {
 
         axios.get('http://127.0.0.1:8000/api/list')
             .then(function (response) {
@@ -20,58 +36,64 @@ const ProductList = () => {
             }).catch(function (error) {
                 console.log(error.message)
             })
-    }, [])
+    }
 
 
     return (
-        <div className='container mx-auto text-center justify-center'>
+        <>
             <Header />
 
-            <h1>Product Listing</h1>
+            <div className='container mx-auto text-center justify-center'>
 
-            <div className='m-10'>
+                <h1>Product Listing</h1>
 
-                <Loading loading={loading} />
+                <div className='m-10'>
 
-                <table >
-                    <tbody>
-                        <tr >
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>File</th>
-                            <th>Description</th>
-                            <th>Price</th>
+                    <Loading loading={loading} />
 
-                        </tr>
-                        {
-                            productlists && productlists.map(productlist => {
-                                const { id, name, file, description, price } = productlist;
-                                let pathList = `http://127.0.0.1:8000/`
-                                return (
+                    <table >
+                        <tbody>
+                            <tr >
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>File</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                                <th>Operations</th>
 
-                                    < tr key={id}>
-                                        <td>{id}</td>
-                                        <td>{name}</td>
-                                        <td> <img src={`${pathList}` + productlist.file_path} alt="" className='w-12' /></td>
-                                        <td>{description}</td>
-                                        <td>{price}</td>
-                                    </tr>
+                            </tr>
+                            {
+                                productlists && productlists.map(productlist => {
+                                    const { id, name, file, description, price } = productlist;
+                                    let pathList = `http://127.0.0.1:8000/`
+                                    return (
 
+                                        <tr key={id}>
+                                            <td>{id}</td>
+                                            <td>{name}</td>
+                                            <td> <img src={`${pathList}` + productlist.file_path} alt="" className='w-12' /></td>
+                                            <td>{description}</td>
+                                            <td>{price}</td>
+                                            <td><span onClick={() => deleteOperation(id)} className='delete-btn'>Delete</span></td>
+                                        </tr>
+
+                                    )
+
+                                }
                                 )
-
                             }
-                            )
-                        }
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
 
 
 
 
-            </div>
+                </div>
 
-        </div >
+            </div >
+        </>
+
     )
 }
 
